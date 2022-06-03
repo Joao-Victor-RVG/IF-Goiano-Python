b01043943db03f47ebf4c5d0622fdeae3aaa2e6b
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2022-06-03 14:37:45 -0300

    commit lista 05

---
+++ Lista 05 IFG/Lista 05/ex001.py
@@ -0,0 +1,30 @@
+print('''
+Digite o combustivel de sua preferencia
+[01] Alcool
+[02] Gasolina
+[03] Diesel
+[04] Fim
+''')
+qtd_alcool = 0
+qtd_gasolina = 0
+qtd_disel = 0
+valor = 0
+
+
+while valor !=4:
+    valor=int(input())
+    if valor ==1:
+        qtd_alcool += 1
+    if valor ==2:
+        qtd_gasolina += 1
+    if valor ==3:
+        qtd_disel += 1
+    if valor ==4:
+        break
+
+print(f''')
+Muito Obrigado!
+Alcool: {qtd_alcool}
+Gasolina: {qtd_gasolina}
+Disel: {qtd_disel}
+''')
---
+++ Lista 05 IFG/Lista 05/ex002.py
@@ -0,0 +1,5 @@
+#print(i , i+1, i+2, 'PUM')
+#i + 4
+n = int(input())
+i = 0
+
