4d8f3f60cc152fa39a0b9b9b69f0fb72cc063ce3
João Victor Rocha Vilela Godoi <joaovictornkz@gmail.com>
2022-07-05 14:27:16 -0300

    Prova

---
+++ Provas/Prova 01/Ex002.py
@@ -0,0 +1,20 @@
+n = int(input())
+sitios = [int(input()) for _ in range(n)]
+count = 0
+print(sum(sitios))
+for i in range(n):
+  if i == 0:
+    if sitios[i] > 0:
+      sitios[i]= sitios[i]-1
+      count+1
+  elif sitios[i]%2 != 0:
+    sitios[i]= sitios[i]-1
+    count+1
+    i-1
+  else:
+    if sitios[i] > 0:
+      sitios[i]= sitios[i]-1
+      count+1
+
+print(len(sitios))
+print(sum(sitios))
